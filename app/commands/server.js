11e5ed791f5a01466839070e57d9e6044309b71a
Aleksandra Godun <a.godun@semrush.com>
2018-02-27 23:40:05 +0300

    Serve static files with express

--- app/commands/server.js
+++ app/commands/server.js
@@ -1,53 +1,18 @@
-const http = require(`http`);
-const url = require(`url`);
-const fs = require(`fs`);
 const path = require(`path`);
-const util = require(`util`);
+const express = require(`express`);
 
-const HOSTNAME = `localhost`;
 const PORT = 3000;
-const EXTENSIONS = {
-  '.css': `text/css`,
-  '.html': `text/html; charset=UTF-8`,
-  '.jpg': `image/jpeg`,
-  '.png': `image/png`,
-  '.ico': `image/x-icon`,
-};
 
-const readFile = util.promisify(fs.readFile);
-
-const sendResponse = async (req, res) => {
-  try {
-    const {pathname} = url.parse(req.url);
-    const staticDir = path.resolve(__dirname, `../../static/`);
-    const filePath = (pathname === `/`) ?
-      `${staticDir}/index.html` :
-      `${staticDir}${pathname}`;
-
-    const data = await readFile(filePath);
-    const ext = path.extname(filePath);
-
-    res.setHeader(`Content-type`, EXTENSIONS[ext]);
-    res.statusCode = 200;
-    res.statusMessage = `OK`;
-
-    res.end(data);
-  } catch (err) {
-    res.writeHead(500, `Internal Server Error`, {
-      'Content-type': `text/plain`
-    });
-    res.end(`OOPS! Something went wrong`);
-  }
-};
-
-const server = http.createServer(sendResponse);
+const app = express();
+const staticDir = path.resolve(__dirname, `../../static/`);
+app.use(express.static(staticDir));
 
 module.exports = {
   name: `server`,
   description: `Запускает сервер`,
   execute() {
-    server.listen(PORT, HOSTNAME, () => {
-      console.log(`Server is running on ${HOSTNAME}:${PORT}`);
+    app.listen(3000, () => {
+      console.log(`Server is running on localhost:${PORT}`);
     });
   }
 };
