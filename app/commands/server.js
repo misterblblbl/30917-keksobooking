fab93d115770779947e67e34a6847a9476109ec6
Aleksandra Godun <a.godun@semrush.com>
2018-03-14 17:59:19 +0300

    Pass env variables

--- app/commands/server.js
+++ app/commands/server.js
@@ -1,13 +1,14 @@
-const {app} = require(`../server`);
+require(`dotenv`).config();
 
-const PORT = 3000;
+const {app} = require(`../server`);
+const {SERVER_PORT, SERVER_HOST} = process.env;
 
 module.exports = {
   name: `server`,
   description: `Запускает сервер`,
   execute() {
-    app.listen(PORT, () => {
-      console.log(`Server is running on localhost:${PORT}`);
+    app.listen(SERVER_PORT, () => {
+      console.log(`Server is running on ${SERVER_HOST}:${SERVER_PORT}`);
     });
   },
   app,
